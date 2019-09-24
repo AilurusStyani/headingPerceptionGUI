f5c53a12418dd0db259c7afcb9e90371803f556f
softdrinks <boyce_bao@163.com>
2019-09-24 14:50:03 +0800

    V1.0
    Update for behavior analyzer

--- backup/breakcheck.m
+++
@@ -1,3 +0,0 @@
-function breakFlag = breakcheck(px,py,fixationPosition,checkWindowP)
-breakFlag = px > fixationPosition(1)+checkWindowP || px < fixationPosition(1)-checkWindowP || py > fixationPosition(2)+checkWindowP || py < fixationPosition(2)-checkWindowP;
-end
